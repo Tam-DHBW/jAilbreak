6a3eca74afcc84098539e2a979f376494302379e
Tam-DHBW <191460049+Tam-DHBW@users.noreply.github.com>
2025-10-14 10:45:29 +0200

    Add tag to all terraform resources

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,13 +7,19 @@ terraform {
   }
 
   backend "s3" {
-    region = "eu-central-1"
-    bucket = "jailbreak-terraform-state"
-    key = "default.tfstate"
+    region       = "eu-central-1"
+    bucket       = "jailbreak-terraform-state"
+    key          = "default.tfstate"
     use_lockfile = true
   }
 }
 
 provider "aws" {
   region = var.aws_region
+
+  default_tags {
+    tags = {
+      TerraformManaged = true
+    }
+  }
 }
